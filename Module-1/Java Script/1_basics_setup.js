c5cea27c39c7f2f648f520fe6155660977e87bb2
ashleshapasula <ashleshapasula@gmail.com>
2025-05-28 14:47:10 +0530

    Delete Module-1 directory

--- Module-1/Java Script/1_basics_setup.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Basics & Setup
-console.log("Welcome to the Community Portal");
-
-window.onload = () => {
-  alert("Page loaded successfully!");
-};
--- Module-1/Java Script/7_dom_manipulation.js
+++
@@ -1,9 +0,0 @@
-// DOM Manipulation
-const container = document.querySelector("#eventContainer");
-
-function renderEvent(name) {
-  const card = document.createElement("div");
-  card.className = "eventCard";
-  card.textContent = name;
-  container.appendChild(card);
-}
